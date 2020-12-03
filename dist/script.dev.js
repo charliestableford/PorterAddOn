4b42721fa41f5f5a05f005ecdcc54c245f7976cf
Charlie Stableford <charliestableford2@gmail.com>
2020-12-03 13:51:52 -0800

    html binding

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -8,10 +8,9 @@ console.log('in'); // $(function(){
 // })
 
 $(function () {
-  var viewModel = function viewModel() {
-    var self = this;
-    self.listitems = ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']);
+  var viewModel = {
+    listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']),
+    listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.', 'Cancel for any reason, up to 24 hours prior to departure.', 'Option available at time of booking or within 24 hours online.', 'Get your refund online. No claim or form required.', 'Additional details in <a href="#" aria-label="Open" tabindex="0">Terms & Conditions</a>'])
   };
-
-  ko.applyBindings(new viewModel());
+  ko.applyBindings(viewModel);
 });
