f21faf442035353c52310da939f72aca93db36d9
Charlie Stableford <charliestableford2@gmail.com>
2020-12-03 12:19:46 -0800

    viewmodel test

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,7 +1,17 @@
 "use strict";
 
-$(function () {
-  var viewModel = function viewModel() {};
+console.log('in'); // $(function(){
+//     var viewModel = function(){
+//         this.showDropDown = ko.observable(false);
+//     }
+//     ko.applyBindings(new viewModel());
+// })
 
-  ko.applyBindings(viewModel);
+$(function () {
+  var viewModel = function viewModel() {
+    var self = this;
+    self.listitems = ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']);
+  };
+
+  ko.applyBindings(new viewModel());
 });
--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+console.log('in')
+
+// $(function(){
+//     var viewModel = function(){
+//         this.showDropDown = ko.observable(false);
+//     }
+//     ko.applyBindings(new viewModel());
+// })
+
 $(function(){
     var viewModel = function(){
-
-    }
-    ko.applyBindings(viewModel);
-})
+    var self = this;
+    self.listitems = ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.'])
+}
+ko.applyBindings(new viewModel())
+}
+)
