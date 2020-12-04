b717bef870b9e59c957138e100afb9932bf96896
Charlie Stableford <charliestableford2@gmail.com>
2020-12-04 12:13:22 -0800

    still having problems with applyBindings

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,23 +1,29 @@
 "use strict";
 
-console.log('in'); // $(function(){
-//     var viewModel = function(){
-//         this.showDropDown = ko.observable(false);
-//     }
-//     ko.applyBindings(new viewModel());
-// })
-//lists
-
+console.log('in');
 $(function () {
   var viewModel1 = {
+    value: ko.observable('details'),
+    options: [{
+      text: 'dets',
+      value: 'dets'
+    }, {
+      text: 'more details',
+      value: 'more details'
+    }]
+  };
+  ko.applyBindings(viewModel1);
+}); //lists
+
+$(function () {
+  var viewModel2 = {
     listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']),
     listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.', 'Cancel for any reason, up to 24 hours prior to departure.', 'Option available at time of booking or within 24 hours online.', 'Get your refund online. No claim or form required.', ''])
-  }; // ko.cleanNode(document.getElementById('viewModel1'));
-
-  ko.applyBindings(viewModel1, document.getElementById('viewModel1')); // ko.cleanNode($element[0]);
+  };
+  ko.applyBindings(viewModel2, document.getElementById('viewModel2'));
 }); // terms and conditions
 // $(function(){
-// var viewModel2 = new function ViewModel() {
+// var viewModel3 = new function ViewModel() {
 //     //false onload
 //     this.shouldShow = ko.observable(false);
 //     this.checkIfShow = function() {
@@ -25,14 +31,14 @@ $(function () {
 //       return this.shouldShow();
 //     };
 //   };
-//   ko.applyBindings(viewModel2, document.getElementById('viewModel2'));
+//   ko.applyBindings(viewModel3, document.getElementById('viewModel3'));
 //   //this feels redundant? could I do an if/ifnot in here instead?
 //   $('#toggle').on('click', function() {
 //     // if not showing then it will on toggle click
-//     viewModel2.shouldShow(!viewModel2.shouldShow());
+//     viewModel3.shouldShow(!viewModel3.shouldShow());
 //   });
 //   $('#toggleTwo').on('click', function() {
-//     viewModel2.shouldShow(!viewModel2.shouldShow());
+//     viewModel3.shouldShow(!viewModel3.shouldShow());
 //   });
 // })
 //buttons
