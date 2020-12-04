a20889ba5a83a6f0654aea38893ee6a0ebfb011b
Charlie Stableford <charliestableford2@gmail.com>
2020-12-04 12:19:49 -0800

    details

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -5,23 +5,26 @@ $(function () {
   var viewModel1 = {
     value: ko.observable('details'),
     options: [{
-      text: 'dets',
-      value: 'dets'
+      text: 'View details',
+      value: 'View details'
     }, {
-      text: 'more details',
-      value: 'more details'
+      text: 'More details',
+      value: 'More details'
+    }, {
+      text: 'Even more details',
+      value: 'Even more details'
     }]
   };
   ko.applyBindings(viewModel1);
 }); //lists
-
-$(function () {
-  var viewModel2 = {
-    listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']),
-    listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.', 'Cancel for any reason, up to 24 hours prior to departure.', 'Option available at time of booking or within 24 hours online.', 'Get your refund online. No claim or form required.', ''])
-  };
-  ko.applyBindings(viewModel2, document.getElementById('viewModel2'));
-}); // terms and conditions
+// $(function(){
+//     var viewModel2 = {
+//     listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']), 
+//     listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.','Cancel for any reason, up to 24 hours prior to departure.','Option available at time of booking or within 24 hours online.','Get your refund online. No claim or form required.', ''])
+//     }
+// ko.applyBindings(viewModel2, document.getElementById('viewModel2'));
+// });
+// terms and conditions
 // $(function(){
 // var viewModel3 = new function ViewModel() {
 //     //false onload
