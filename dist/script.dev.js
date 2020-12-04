960ad55079416fea330e18649e7b07b0fb40f655
Charlie Stableford <charliestableford2@gmail.com>
2020-12-04 09:05:54 -0800

    testing

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -7,14 +7,14 @@ console.log('in'); // $(function(){
 //     ko.applyBindings(new viewModel());
 // })
 //lists
-
-$(function () {
-  var viewModel = {
-    listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']),
-    listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.', 'Cancel for any reason, up to 24 hours prior to departure.', 'Option available at time of booking or within 24 hours online.', 'Get your refund online. No claim or form required.', 'Additional details in <a href="#" aria-label="Open" tabindex="0">Terms & Conditions</a>'])
-  };
-  ko.applyBindings(viewModel);
-}); //buttons
+// $(function(){
+//     var viewModel = {
+//     listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']), 
+//     listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.','Cancel for any reason, up to 24 hours prior to departure.','Option available at time of booking or within 24 hours online.','Get your refund online. No claim or form required.', 'Additional details in <a href="#" aria-label="Open" tabindex="0">Terms & Conditions</a>'])
+//     }
+// ko.applyBindings(viewModel);
+// });
+//buttons
 // $(function(){
 //     var viewModel = {
 //         flexOne: function(formElement){
@@ -29,10 +29,15 @@ $(function () {
 //terms and conditions
 
 $(function () {
-  var viewModel = {
-    showTerms: ko.observable(false)
-  };
-  var vm = new viewModel();
-  ko.applyBindings(vm);
-  vm.showTerms(true);
+  var viewModel = new function VM() {
+    this.shouldShow = ko.observable(false);
+
+    this.checkIfShow = function () {
+      return this.shouldShow();
+    };
+  }();
+  ko.applyBindings(viewModel);
+  $('#toggle').on('click', function () {
+    viewModel.shouldShow(!viewModel.shouldShow());
+  });
 });
--- script.js
+++ script.js
@@ -8,13 +8,13 @@ console.log('in')
 // })
 
 //lists
-$(function(){
-    var viewModel = {
-    listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']), 
-    listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.','Cancel for any reason, up to 24 hours prior to departure.','Option available at time of booking or within 24 hours online.','Get your refund online. No claim or form required.', 'Additional details in <a href="#" aria-label="Open" tabindex="0">Terms & Conditions</a>'])
-    }
-ko.applyBindings(viewModel);
-});
+// $(function(){
+//     var viewModel = {
+//     listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']), 
+//     listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.','Cancel for any reason, up to 24 hours prior to departure.','Option available at time of booking or within 24 hours online.','Get your refund online. No claim or form required.', 'Additional details in <a href="#" aria-label="Open" tabindex="0">Terms & Conditions</a>'])
+//     }
+// ko.applyBindings(viewModel);
+// });
 
 //buttons
 // $(function(){
@@ -35,11 +35,17 @@ ko.applyBindings(viewModel);
 
 //terms and conditions
 $(function(){
-    var viewModel = {
-        showTerms: ko.observable(false)
-    }
-    var vm = new viewModel();
+var viewModel = new function VM() {
+    this.shouldShow = ko.observable(false);
     
-    ko.applyBindings(vm)
-    vm.showTerms(true);
+    this.checkIfShow = function() {
+      return this.shouldShow();
+    };
+  };
+  
+  ko.applyBindings(viewModel);
+  
+  $('#toggle').on('click', function() {
+    viewModel.shouldShow(!viewModel.shouldShow());
+  });
 })
