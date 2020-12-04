0bbffe9a9883ad6168952d72f4a429f85b13cb8f
Charlie Stableford <charliestableford2@gmail.com>
2020-12-04 11:18:20 -0800

    clean node?

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -12,30 +12,30 @@ $(function () {
   var viewModel1 = {
     listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']),
     listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.', 'Cancel for any reason, up to 24 hours prior to departure.', 'Option available at time of booking or within 24 hours online.', 'Get your refund online. No claim or form required.', 'Additional details in <a href="#" aria-label="Open" tabindex="0">Terms & Conditions</a>'])
-  };
-  ko.applyBindings(viewModel1); // ko.cleanNode($element[0]);
+  }; // ko.cleanNode(document.getElementById('viewModel1'));
+
+  ko.applyBindings(viewModel1, document.getElementById('viewModel1')); // ko.cleanNode($element[0]);
 }); // terms and conditions
-
-$(function () {
-  var viewModel2 = new function ViewModel() {
-    //false onload
-    this.shouldShow = ko.observable(false);
-
-    this.checkIfShow = function () {
-      //is it visible
-      return this.shouldShow();
-    };
-  }();
-  ko.applyBindings(viewModel2); //this feels redundant? could I do an if/ifnot in here instead?
-
-  $('#toggle').on('click', function () {
-    // if not showing then it will on toggle click
-    viewModel2.shouldShow(!viewModel2.shouldShow());
-  });
-  $('#toggleTwo').on('click', function () {
-    viewModel2.shouldShow(!viewModel2.shouldShow());
-  });
-}); //buttons
+// $(function(){
+// var viewModel2 = new function ViewModel() {
+//     //false onload
+//     this.shouldShow = ko.observable(false);
+//     this.checkIfShow = function() {
+//         //is it visible
+//       return this.shouldShow();
+//     };
+//   };
+//   ko.applyBindings(viewModel2, document.getElementById('viewModel2'));
+//   //this feels redundant? could I do an if/ifnot in here instead?
+//   $('#toggle').on('click', function() {
+//     // if not showing then it will on toggle click
+//     viewModel2.shouldShow(!viewModel2.shouldShow());
+//   });
+//   $('#toggleTwo').on('click', function() {
+//     viewModel2.shouldShow(!viewModel2.shouldShow());
+//   });
+// })
+//buttons
 // $(function(){
 //     var viewModel = {
 //         flexOne: function(formElement){
