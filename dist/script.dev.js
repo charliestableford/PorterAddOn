a34a20bf55478f0c700fae313ce5f7337104293a
Charlie Stableford <charliestableford2@gmail.com>
2020-12-04 11:02:08 -0800

    figuring out bindings

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -7,14 +7,35 @@ console.log('in'); // $(function(){
 //     ko.applyBindings(new viewModel());
 // })
 //lists
-// $(function(){
-//     var viewModel = {
-//     listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']), 
-//     listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.','Cancel for any reason, up to 24 hours prior to departure.','Option available at time of booking or within 24 hours online.','Get your refund online. No claim or form required.', 'Additional details in <a href="#" aria-label="Open" tabindex="0">Terms & Conditions</a>'])
-//     }
-// ko.applyBindings(viewModel);
-// });
-//buttons
+
+$(function () {
+  var viewModel = {
+    listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']),
+    listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.', 'Cancel for any reason, up to 24 hours prior to departure.', 'Option available at time of booking or within 24 hours online.', 'Get your refund online. No claim or form required.', 'Additional details in <a href="#" aria-label="Open" tabindex="0">Terms & Conditions</a>'])
+  };
+  ko.applyBindings(viewModel); // ko.cleanNode($element[0]);
+}); // terms and conditions
+
+$(function () {
+  var viewModel = new function ViewModel() {
+    //false onload
+    this.shouldShow = ko.observable(false);
+
+    this.checkIfShow = function () {
+      //is it visible
+      return this.shouldShow();
+    };
+  }(); //   ko.applyBindings(viewModel);
+  //this feels redundant? could I do an if/ifnot in here instead?
+
+  $('#toggle').on('click', function () {
+    // if not showing then it will on toggle click
+    viewModel.shouldShow(!viewModel.shouldShow());
+  });
+  $('#toggleTwo').on('click', function () {
+    viewModel.shouldShow(!viewModel.shouldShow());
+  });
+}); //buttons
 // $(function(){
 //     var viewModel = {
 //         flexOne: function(formElement){
@@ -26,22 +47,3 @@ console.log('in'); // $(function(){
 //     }
 //     ko.applyBindings(viewModel);
 // })
-//terms and conditions
-
-$(function () {
-  var viewModel = new function ViewModel() {
-    this.shouldShow = ko.observable(false);
-
-    this.checkIfShow = function () {
-      return this.shouldShow();
-    };
-  }();
-  ko.applyBindings(viewModel); //this feels redundant? could I do an if/ifnot in here instead?
-
-  $('#toggle').on('click', function () {
-    viewModel.shouldShow(!viewModel.shouldShow());
-  });
-  $('#toggleTwo').on('click', function () {
-    viewModel.shouldShow(!viewModel.shouldShow());
-  });
-});
--- script.js
+++ script.js
@@ -8,13 +8,38 @@ console.log('in')
 // })
 
 //lists
-// $(function(){
-//     var viewModel = {
-//     listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']), 
-//     listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.','Cancel for any reason, up to 24 hours prior to departure.','Option available at time of booking or within 24 hours online.','Get your refund online. No claim or form required.', 'Additional details in <a href="#" aria-label="Open" tabindex="0">Terms & Conditions</a>'])
-//     }
-// ko.applyBindings(viewModel);
-// });
+$(function(){
+    var viewModel = {
+    listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']), 
+    listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.','Cancel for any reason, up to 24 hours prior to departure.','Option available at time of booking or within 24 hours online.','Get your refund online. No claim or form required.', 'Additional details in <a href="#" aria-label="Open" tabindex="0">Terms & Conditions</a>'])
+    }
+    
+ko.applyBindings(viewModel);
+// ko.cleanNode($element[0]);
+});
+
+// terms and conditions
+$(function(){
+var viewModel = new function ViewModel() {
+    //false onload
+    this.shouldShow = ko.observable(false);
+    
+    this.checkIfShow = function() {
+        //is it visible
+      return this.shouldShow();
+    };
+  };
+//   ko.applyBindings(viewModel);
+  //this feels redundant? could I do an if/ifnot in here instead?
+  $('#toggle').on('click', function() {
+    // if not showing then it will on toggle click
+    viewModel.shouldShow(!viewModel.shouldShow());
+  });
+  $('#toggleTwo').on('click', function() {
+    viewModel.shouldShow(!viewModel.shouldShow());
+  });
+})
+
 
 //buttons
 // $(function(){
@@ -32,23 +57,3 @@ console.log('in')
 //     }
 //     ko.applyBindings(viewModel);
 // })
-
-//terms and conditions
-$(function(){
-var viewModel = new function ViewModel() {
-    this.shouldShow = ko.observable(false);
-    
-    this.checkIfShow = function() {
-      return this.shouldShow();
-    };
-  };
-  
-  ko.applyBindings(viewModel);
-  //this feels redundant? could I do an if/ifnot in here instead?
-  $('#toggle').on('click', function() {
-    viewModel.shouldShow(!viewModel.shouldShow());
-  });
-  $('#toggleTwo').on('click', function() {
-    viewModel.shouldShow(!viewModel.shouldShow());
-  });
-})
