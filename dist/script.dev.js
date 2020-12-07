db88a12a8c162892a33c8c0256f5c4b3cc9190e0
Charlie Stableford <charliestableford2@gmail.com>
2020-12-07 09:52:43 -0800

    one lump vm

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,8 +1,9 @@
 "use strict";
 
-console.log('in');
+console.log('in'); //flatten into one viewmodel
+
 $(function () {
-  var viewModel1 = {
+  var viewModel = {
     value: ko.observable('details'),
     options: [{
       text: 'View details',
@@ -13,38 +14,32 @@ $(function () {
     }, {
       text: 'Even more details',
       value: 'Even more details'
-    }]
+    }],
+    listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']),
+    listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.', 'Cancel for any reason, up to 24 hours prior to departure.', 'Option available at time of booking or within 24 hours online.', 'Get your refund online. No claim or form required.', ''])
   };
-  $(function () {
-    var viewModel2 = {
-      listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']),
-      listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.', 'Cancel for any reason, up to 24 hours prior to departure.', 'Option available at time of booking or within 24 hours online.', 'Get your refund online. No claim or form required.', ''])
-    };
-  });
-  $(function () {
-    var viewModel3 = new function ViewModel() {
-      //false onload
-      this.shouldShow = ko.observable(false);
-
-      this.checkIfShow = function () {
-        //is it visible
-        return this.shouldShow();
-      };
-    }();
-    ko.applyBindings(viewModel3, document.getElementById('viewModel3')); //this feels redundant? could I do an if/ifnot in here instead?
-
-    $('#toggle').on('click', function () {
-      // if not showing then it will on toggle click
-      viewModel3.shouldShow(!viewModel3.shouldShow());
-    });
-    $('#toggleTwo').on('click', function () {
-      viewModel3.shouldShow(!viewModel3.shouldShow());
-    });
-  });
-  ko.applyBindings(viewModel1, document.getElementById('viewModel1'));
-  ko.applyBindings(viewModel2, document.getElementById('viewModel2'));
-  ko.applyBindings(viewModel3, document.getElementById('viewModel3'));
-}); //lists
+  ko.applyBindings(viewModel);
+}); // var viewModel3 = new function ViewModel() {
+//     //false onload
+//     this.shouldShow = ko.observable(false);
+//     this.checkIfShow = function() {
+//         //is it visible
+//         return this.shouldShow();
+//     };
+//     };
+//     $('#toggle').on('click', function() {
+//     // if not showing then it will on toggle click
+//     viewModel3.shouldShow(!viewModel3.shouldShow());
+//     });
+//     $('#toggleTwo').on('click', function() {
+//     viewModel3.shouldShow(!viewModel3.shouldShow());
+//     });
+// })
+// ko.applyBindings(viewModel));
+// ko.applyBindings(viewModel2, document.getElementById('viewModel2'));
+// ko.applyBindings(viewModel3, document.getElementById('viewModel3'));
+// })
+//lists
 // $(function(){
 //     var viewModel2 = {
 //     listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']), 
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 console.log('in')
+//flatten into one viewmodel
 
     $(function(){
-        var viewModel1 = {
+        var viewModel = {
             value: ko.observable('details'), 
             options: [{
                 text: 'View details',
@@ -14,40 +15,38 @@ console.log('in')
             {
                 text: 'Even more details',
                 value: 'Even more details'
-            }]
+            }],
 
-        }
-        $(function(){
-             var viewModel2 = {
             listItemOne: ko.observableArray(['Available for all non-refundable fares.', 'Cancel and refund up to 24 hours prior to departure.', 'Add to reservations up to 1 day after booking.', 'Refund online, with no claims or forms.']), 
             listItemTwo: ko.observableArray(['Get a full travel refund, even on a non-refundable fare.','Cancel for any reason, up to 24 hours prior to departure.','Option available at time of booking or within 24 hours online.','Get your refund online. No claim or form required.', ''])
-            }
-        })
+            
+        }
+        ko.applyBindings(viewModel)
+    });
+    
        
-            $(function(){
-                var viewModel3 = new function ViewModel() {
-                    //false onload
-                    this.shouldShow = ko.observable(false);
-                    
-                    this.checkIfShow = function() {
-                        //is it visible
-                      return this.shouldShow();
-                    };
-                  };
-                  ko.applyBindings(viewModel3, document.getElementById('viewModel3'));
-                  //this feels redundant? could I do an if/ifnot in here instead?
-                  $('#toggle').on('click', function() {
-                    // if not showing then it will on toggle click
-                    viewModel3.shouldShow(!viewModel3.shouldShow());
-                  });
-                  $('#toggleTwo').on('click', function() {
-                    viewModel3.shouldShow(!viewModel3.shouldShow());
-                  });
-                })
-        ko.applyBindings(viewModel1, document.getElementById('viewModel1'));
-        ko.applyBindings(viewModel2, document.getElementById('viewModel2'));
-        ko.applyBindings(viewModel3, document.getElementById('viewModel3'));
-    })
+        // var viewModel3 = new function ViewModel() {
+        //     //false onload
+        //     this.shouldShow = ko.observable(false);
+            
+        //     this.checkIfShow = function() {
+        //         //is it visible
+        //         return this.shouldShow();
+        //     };
+        //     };
+
+        //     $('#toggle').on('click', function() {
+        //     // if not showing then it will on toggle click
+        //     viewModel3.shouldShow(!viewModel3.shouldShow());
+        //     });
+        //     $('#toggleTwo').on('click', function() {
+        //     viewModel3.shouldShow(!viewModel3.shouldShow());
+        //     });
+        // })
+        // ko.applyBindings(viewModel));
+        // ko.applyBindings(viewModel2, document.getElementById('viewModel2'));
+        // ko.applyBindings(viewModel3, document.getElementById('viewModel3'));
+    // })
 
 //lists
 // $(function(){
